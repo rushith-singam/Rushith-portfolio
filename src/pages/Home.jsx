0304421c771174e049dda95354c8a9aec6dc95c7
rushith-singam <rushithsingamsetty8@gmail.com>
2026-06-22 00:10:44 +0530

    Updated React project

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,12 @@
-import Navbar from "../components/Navbar";
-import Hero from "../components/Hero";
-import About from "../components/About";
-import Skills from "../components/Skills";
-import Experience from "../components/Experience";
-import Projects from "../components/Projects";
-import Certifications from "../components/Certifications";
-import Contact from "../components/Contact";
-import Footer from "../components/Footer";
+import Navbar from "../components/navbar";
+import Hero from "../components/hero";
+import About from "../components/about";
+import Skills from "../components/skills";
+import Experience from "../components/experience";
+import Projects from "../components/projects";
+import Certifications from "../components/certifications";
+import Contact from "../components/contact";
+import Footer from "../components/footer";
 
 function Home() {
   return (
